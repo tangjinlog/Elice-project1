8fb6de96f59c4d13caf3aa621b4f3767e7aa1a3a
Donghyun Kim <crtmt97@gmail.com>
2022-11-02 01:19:41 +0900

    반복문으로 모든 체크박스에 이벤트 리스너 추가

--- src/views/goods/goods.js
+++ src/views/goods/goods.js
@@ -1,21 +1,46 @@
 //요소 모음
 const option_list = document.getElementById('option_list');
-const testCheck = document.querySelector('.test');
-const freeDeli = document.querySelector('.free_deli');
 const searchBtn = document.querySelector('.search-btn');
+const filter_options = document.querySelectorAll('.filter-option');
 
 //이벤트추가
-freeDeli.addEventListener('click', addOption);
+filter_options.forEach((a) => {
+  a.addEventListener('change', addOption);
+});
 searchBtn.addEventListener('click', searchGoods);
+
+const filter_option = {
+  free_delivery: false,
+  color_white: false,
+  color_gray: false,
+  color_black: false,
+  color_chromatic: false,
+  under_10000: false,
+  over_10000: false,
+  over_20000: false,
+};
+//필터옵션은 리스트에 담아서
+//체크된 옵션은 true, 아니면 false로 하는 방법도 있음
 
 //이벤트리스너 함수
 function addOption(e) {
-  option_list.insertAdjacentHTML(
-    'beforeend',
-    `
-      <li class="mx-2 "><span class="bg-gray-400 rounded-lg text-xs p-1">${e.target.innerText}</span> <button class="">x</button></li>
-    `
-  );
+  console.log(e.target);
+  if (e.target.checked) {
+    option_list.insertAdjacentHTML(
+      'beforeend',
+      `
+        <li class="mx-2 ${e.target.value} "><label for=${e.target.value}  class="bg-gray-400 rounded-lg text-xs p-1">${e.target.value}</label> <button id=${e.target.value} >x</button></li>
+      `
+    );
+  } else {
+    // option_list.insertAdjacentHTML(
+    //   'beforeend',
+    //   `
+    //     <li class=`mx-2 `><label class="bg-gray-400 rounded-lg text-xs p-1">${e.target.value}</label> <button id=${e.target.innerText} >x</button></li>
+    //   `
+    // );
+  }
+  console.log(e.target.value);
 }
 
 async function searchGoods(e) {
