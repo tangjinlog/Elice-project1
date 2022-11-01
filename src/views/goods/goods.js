553d2ad11874d9e2fc186fde4bbd36e720595e6e
Donghyun Kim <crtmt97@gmail.com>
2022-11-01 19:28:28 +0900

    클래스명 변경 및 자잘한 수정사항

--- src/views/goods/goods.js
+++ src/views/goods/goods.js
@@ -2,14 +2,13 @@
 const option_list = document.getElementById('option_list');
 const testCheck = document.querySelector('.test');
 const freeDeli = document.querySelector('.free_deli');
+const searchBtn = document.querySelector('.search-btn');
 
 //이벤트추가
-testCheck.addEventListener('click', (e) => {
-  alert('clicked');
-});
-
 freeDeli.addEventListener('click', addOption);
+searchBtn.addEventListener('click', searchGoods);
 
+//이벤트리스너 함수
 function addOption(e) {
   option_list.insertAdjacentHTML(
     'beforeend',
@@ -17,4 +16,8 @@ function addOption(e) {
       <li class="mx-2 "><span class="bg-gray-400 rounded-lg text-xs p-1">${e.target.innerText}</span> <button class="">x</button></li>
     `
   );
+}
+
+async function searchGoods(e) {
+  e.preventDefault();
 }
