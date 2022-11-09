9c05d5cb7b48c4aeab079a66ce9445c527a5af51
Donghyun Kim <crtmt97@gmail.com>
2022-11-09 16:09:22 +0900

    viewRouter 처리

--- src/routers/views-router.js
+++ src/routers/views-router.js
@@ -3,16 +3,23 @@ import path from 'path';
 
 const viewsRouter = express.Router();
 
-// 페이지별로 html, css, js 파일들을 라우팅함
-// 아래와 같이 하면, http://localhost:5000/ 에서는 views/home/home.html 파일을,
-// http://localhost:5000/register 에서는 views/register/register.html 파일을 화면에 띄움
 viewsRouter.use('/', serveStatic('home'));
+viewsRouter.use('/account/addProduct', serveAccountStatic(`add`));
+viewsRouter.use('/account/addCategory', serveAccountStatic(`orders`));
+viewsRouter.use('/account/orderManagement', serveAccountStatic(`security`));
+viewsRouter.use('/account/userManagement', serveAccountStatic(`signout`));
+viewsRouter.use('/admin/addProduct', serveAdminStatic(`addProduct`));
+viewsRouter.use('/admin/addCategory', serveAdminStatic(`addCategory`));
+viewsRouter.use('/admin/orderManagement', serveAdminStatic(`orderManagement`));
+viewsRouter.use('/admin/userManagement', serveAdminStatic(`userManagement`));
+viewsRouter.use('/cart', serveStatic('cart'));
+viewsRouter.use('/complete', serveStatic('complete'));
+viewsRouter.use('/goods', serveStatic('goods'));
+viewsRouter.use('/goods-detail/:id', serveStatic('goods-detail'));
 viewsRouter.use('/register', serveStatic('register'));
 viewsRouter.use('/login', serveStatic('login'));
 viewsRouter.use('/admin', serveStatic('admin'));
-viewsRouter.use('/admin/addProduct', serveAdminStatic(`addProduct`));
-viewsRouter.use('/admin/addCategory', serveAdminStatic(`addCategory`));
-viewsRouter.use('/goods-detail/:id', serveStatic('goods-detail'));
+viewsRouter.use('/shippingpoint', serveStatic('shippingpoint'));
 
 // views 폴더의 최상단 파일인 rabbit.png, api.js 등을 쓸 수 있게 함
 viewsRouter.use('/', serveStatic(''));
@@ -26,6 +33,11 @@ function serveStatic(resource) {
 }
 function serveAdminStatic(resource) {
 	const resourcePath = path.join(__dirname, `../views/admin/${resource}`);
+	const option = { index: `${resource}.html` };
+	return express.static(resourcePath, option);
+}
+function serveAccountStatic(resource) {
+	const resourcePath = path.join(__dirname, `../views/account/${resource}`);
 	const option = { index: `${resource}.html` };
 	return express.static(resourcePath, option);
 }
