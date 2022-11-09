d53f372a8ff523c4e3ac191e6fe40bc74d432093
Donghyun Kim <crtmt97@gmail.com>
2022-11-09 10:43:06 +0900

    [feature]장바구니 플로우 완성

--- src/views/goods/goods.js
+++ src/views/goods/goods.js
@@ -1,98 +1,48 @@
 import { navTemplate } from '/common/nav.js';
+const $ = (selector) => document.querySelector(selector);
+
 /* nav Template */
 function addNav() {
 	const header = document.querySelector('.headerNav');
 	header.innerHTML = navTemplate();
 }
 addNav();
-//요소 모음
-const option_list = document.getElementById('option_list');
-const searchBtn = document.querySelector('.search-btn');
-const filter_options = document.querySelectorAll('.filter-option');
 
-//이벤트추가
-filter_options.forEach((inputValue) => {
-	inputValue.addEventListener('change', addOption);
-});
-searchBtn.addEventListener('click', searchGoods);
+const loadAllProducts = async () => {
+	const response = await fetch('/api/productlist');
+	const productDatas = await response.json();
+	const productList = document.querySelector('.product-list');
 
-const filter_option = [];
-//필터옵션은 리스트에 담아서
-//체크된 옵션은 true, 아니면 false로 하는 방법도 있음
-
-//이벤트리스너 함수
-function addOption(e) {
-	console.log(e.target);
-	if (e.target.checked) {
-		option_list.insertAdjacentHTML(
+	productDatas.forEach((product) => {
+		productList.insertAdjacentHTML(
 			'beforeend',
 			`
-        <li class="mx-2 ${e.target.value} "><label for=${e.target.value}  class="bg-gray-400 rounded-lg text-xs p-1">${e.target.value}</label> <button id=${e.target.value} >x</button></li>
-      `,
+	  <a href="../goods-detail/goods-detail.html">
+		  <div class="productItem flex-col w-full h-full bg-slate-200">
+		    <div class="grow w-full">
+				  <img id="${product._id}" src="${
+				product.image ? product.image : '../images/no-image.png'
+			}" alt="상품이미지">
+		    </div>
+		    <p>${product.name}</p>
+		    <div>
+			    ${product.price}원
+		    </div>
+	    </div>
+		</a>
+  `,
 		);
-	} else {
-		// option_list.insertAdjacentHTML(
-		//   'beforeend',
-		//   `
-		//     <li class=`mx-2 `><label class="bg-gray-400 rounded-lg text-xs p-1">${e.target.value}</label> <button id=${e.target.innerText} >x</button></li>
-		//   `
-		// );
-	}
-	console.log(e.target.value);
-}
-
-async function searchGoods(e) {
-	e.preventDefault();
-}
-
-/* product template */
-function productTemplate(image, name, price) {
-	return `
-    <div class="productItem flex-col w-full h-full bg-slate-200">
-      <div class="grow w-full">
-          <img class="image" src="${image}" alt="상품이미지">
-      </div>
-      <p class="name">${name}</p>
-      <div>
-        <span class="price">${price}원</span>
-      </div>
-    </div>
-  `;
-}
-
-async function productAll() {
-	const response = await fetch('/api/productlist');
-	const result = await response.json();
-	const productCon = document.querySelector('.productCon');
-	console.log(result);
-
-	result.map((e) => {
-		const name = e.name;
-		const price = e.price;
-		const image = e.image ? e.image : '../images/no-image.png';
-		const productItem = productTemplate(image, name, price);
-		let detailArr = [];
-
-		productCon.innerHTML += productItem;
-
-		const images = document.querySelectorAll('.image');
-		images.forEach((e) => {
-			e.addEventListener('click', (e) => {
-				const image = e.target.src;
-				console.log(e.target);
-				const name = e.target.parentNode.nextElementSibling.innerText;
-				const price = parseInt(
-					e.target.parentElement.nextElementSibling.nextElementSibling
-						.firstElementChild.innerText,
-				);
-				console.log(image, name, price);
-				const details = { image, name, price };
-				detailArr.push(details);
-				const value = JSON.stringify(detailArr);
-				window.localStorage.setItem('details', value);
-			});
+	});
+	const productItems = document.querySelectorAll('.productItem');
+	productItems.forEach((productItem) => {
+		productItem.addEventListener('click', (e) => {
+			let productData = productDatas.filter(
+				(product) => product._id == e.target.id,
+			);
+			productData = JSON.stringify(productData);
+			window.localStorage.setItem('detail', productData);
 		});
 	});
-}
+};
 
-productAll();
+loadAllProducts();
