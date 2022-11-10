6d7e3ffa35ad3e11394fe0fb2cd8d3bd631b4b09
sunnao <osa125125@naver.com>
2022-11-10 13:04:48 +0900

    [docs] : product schema 변경 상세설명 추가, 무료배송 삭제

--- src/db/schemas/product-schema.js
+++ src/db/schemas/product-schema.js
@@ -1,25 +1,20 @@
 import { Schema } from 'mongoose';
 
 const ProductSchema = new Schema(
-	{
-		name: { type: String, required: true },
-		size: { type: String, required: true },
-		color: { type: String, required: true },
-		price: { type: Number, required: true },
-		free_delivery: { type: Boolean, required: true },
-		categoryId: {
-			type: Schema.Types.ObjectId,
-			required: true,
-			ref: 'category',
-		},
-		detailDescription: { type: String, required: true },
-		image: { type: String, required: true },
-		stock: { type: Number, default: 1 },
-	},
-	{
-		collection: 'products',
-		timestamps: true,
-	},
+  {
+    name: { type: String, required: true },
+    size: { type: String, required: true },
+    color: { type: String, required: true },
+    price: { type: Number, required: true },
+    category: { type: String, required: true },
+    detailDescription: { type: String, required: true },
+    image: { type: String, required: true },
+    stock: { type: Number, default: 1 },
+  },
+  {
+    collection: 'products',
+    timestamps: true,
+  },
 );
 
 export { ProductSchema };
