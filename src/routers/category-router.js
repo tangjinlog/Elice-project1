92ca92daf2193bb3aad337114b9efe9b184ba55f
Suna Oh <sunna55125@gmail.com>
2022-11-12 13:13:49 +0000

    전태호코치님께 코드질문

--- src/routers/category-router.js
+++ src/routers/category-router.js
@@ -62,6 +62,13 @@ categoryRouter.patch(
 				...(name && { name }),
 				...(description && { description }),
 			};
+			// 코치님! ↑ 이렇게 업데이트 내용을 주는걸 스켈레톤코드에서 보고 적용시켜본건데
+			// 예들들어 업데이트내용으로 name값(뒤에{name})까지 있으면 업데이트값 보내고, 하나라도 없으면 원래값(앞에name)을 updateInfo로 보내는걸로
+			// 이해를 했습니다. 그런데 사실 업데이트로 mongoose의 patch메소드를 쓰면 
+			// 변경요청 받은 속성만 업데이트되고 기재되지 않은 속성은 원래값을 유지하기 때문에 이런 코드는 굳이 필요가 없는것 아닌가요?
+			// 이런 과정이 없어도 업데이트 적용이 잘 되는 것같아 제 코드에선 다른 곳에는 사용하지 않았는데
+			// 이렇게 선언해줄 시 이점이 있는건지 궁금합니다.
+
 
 			// 카테고리 정보 업데이트
 			const updatedCategoryInfo = await categoryService.editCategory(
